b2974a080dfd9751d3c4b02b94ddeaf2cfa50cd3
patiphanz <patiphan.sr@ku.th>
2019-05-03 14:55:38 +0700

    Add 3 fragment in app

--- app/src/main/java/com/patiphan/upoll/PollPage.java
+++ app/src/main/java/com/patiphan/upoll/PollPage.java
@@ -1,44 +1,65 @@
 package com.patiphan.upoll;
 
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class PollPage extends AppCompatActivity {
+public class PollPage extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_friend:
-                    mTextMessage.setText("Friend List");
-                    return true;
-                case R.id.navigation_ulist:
-                    mTextMessage.setText("UList");
-                    return true;
-                case R.id.navigation_profile:
-                    mTextMessage.setText("Profile");
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poll_page);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
     }
 
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+
+            return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_ulist:
+                fragment = new UlistFragment();
+                break;
+
+            case R.id.navigation_friend:
+                fragment = new FriendFragment();
+                break;
+
+
+        }
+
+        return loadFragment(fragment);
+    }
 }
